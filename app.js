023560f886f558ad293af2991e5d95e2ea1a342c
MaxySpark <hello@maxyspark.com>
2017-05-07 20:40:26 +0530

    fix cureent scrapper

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function getMusic (songUrl,songName,songList) {
             else {
                 var $ = cheerio.load(body);
                 var allLinks = [];
-                $("#dl > .d-info2 > dl >dd >a").each(function(){
+                $(".item-9 > table > tbody > tr > td:nth-child(4) > a").each(function(){
                     allLinks.push($(this).attr('href'));
                 });
                 if(allLinks.length < 2) {
